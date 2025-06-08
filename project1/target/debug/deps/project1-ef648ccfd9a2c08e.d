cb4b592f26df0834ef69a7cab738c2e52db05f1e
pauls <pauls@computer.local>
2025-06-07 23:50:11 -0400

    project1

---
+++ project1/target/debug/deps/project1-ef648ccfd9a2c08e.d
@@ -0,0 +1,5 @@
+/Users/pauls/rustPrograms/rustPrograms/project1/target/debug/deps/project1-ef648ccfd9a2c08e: src/main.rs
+
+/Users/pauls/rustPrograms/rustPrograms/project1/target/debug/deps/project1-ef648ccfd9a2c08e.d: src/main.rs
+
+src/main.rs:
---
+++ project1/target/debug/project1.d
@@ -0,0 +1,1 @@
+/Users/pauls/rustPrograms/rustPrograms/project1/target/debug/project1: /Users/pauls/rustPrograms/rustPrograms/project1/src/main.rs
